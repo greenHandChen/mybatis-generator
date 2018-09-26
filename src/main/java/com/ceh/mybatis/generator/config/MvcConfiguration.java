249baf73d8b7ec3e58baa3a3d04d03dc4df1fa0a
enhui.chen <enhui.chen@hand-china.com>
2018-09-26 14:11:00 +0800

    mybatis-generator代码生成器v1.0

--- src/main/java/com/ceh/mybatis/generator/config/MvcConfiguration.java
+++ src/main/java/com/ceh/mybatis/generator/config/MvcConfiguration.java
@@ -1,13 +1,9 @@
 package com.ceh.mybatis.generator.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by enHui.Chen on 2018/9/20.
--- src/main/webapps/Demo.ftl
+++
@@ -1,6 +0,0 @@
-
-public class ${tableName}{
-    <#list fieldInfos as fieldInfo>
-        private String ${fieldInfo.fieldName};
-    </#list>
-}
